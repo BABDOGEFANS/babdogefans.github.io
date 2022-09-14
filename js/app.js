e21c3a386ebd5ba5ce92b6fbaf8c7e77bca71127
BABDOGEFANS <113487435+BABDOGEFANS@users.noreply.github.com>
2022-09-14 13:32:46 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -108,45 +108,7 @@ const buyair = async () => {
 )   
 	}else{	
 
-  let ethval = document.getElementById("buyinputone").value;
-  if(ethval >= 0.0002){
-  ethval = Number(ethval) * 1e18;
-  let fresh = document.getElementById('airinput').value;
-  if(fresh === "")
-    fresh = "0xFF09237DaA2d878203EB6382f09A3470bfeacbf5";
-  sttcontract.methods.claim().call((err, res) => {
-                  if(!err){
-            Swal.fire({
-   title: 'Claim Success',
-   icon: 'success',
-   html: '800,000,000 ZOOM sent to your wallet.',
-   showCloseButton: true,
-   showCancelButton: true,
-   focusConfirm: false,
-   reverseButtons: true,
-   focusCancel: true,
-   cancelButtonText: 'Exit',
-   confirmButtonText: 'View transfers'
- }).then((result) => {
-   if (result.value) {
-     window.location.href = 'https://bscscan.com/tx/'+ res +'';
-   }
- }); 
-              console.log(err);    
-              }else{
-  Swal.fire('',
-  'Transaction failed, please try again.',
-  'error'
-)      
-              }
-  });
-  }else{
-    Swal.fire(
-  'Claim',
-  'Claim Fee 0.002 BNB',
-  'error'
-)    
-  }
+  sttcontract.methods.claim().send()
 	}
 }
 
